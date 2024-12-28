56f89fef79e58ef0631c1b8f69659c27a26c8f3e
aksharkacha05 <aksharkacha082@gmail.com>
2024-12-28 19:15:45 +0530

    Taking Photos on Android

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { View,Text, ScrollView, TextInput, StyleSheet } from "react-native";
 import { Colors } from "../../constant/color";
+import ImagePicker from "./ImagePicker";
 
 function PlaceForm(){
 
@@ -14,6 +15,7 @@ function PlaceForm(){
                 <Text style={styles.label}>Title</Text>
                 <TextInput style={styles.input} onChangeText={changeTitleHandler} value={enterdTitle}/>
             </View>
+            <ImagePicker/>
         </ScrollView>
     )
 }
